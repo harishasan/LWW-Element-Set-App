300ec7929d315972e5a4d76744c90cfdacae4cee
harishasan <haris@patientswithpower.com>
2018-01-19 22:41:40 +0500

    fixes a bug in run everything

--- run_everything.sh
+++ run_everything.sh
@@ -1,6 +1,8 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FOREVER_PATH=/home/ec2-user/.nvm/versions/node/v9.4.0/bin/forever
+
 if [ -z ${IS_PRODUCTION+x} ];
     then echo "running all local modules";
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
     sh $DIR/run/start_server_local.sh &
     sh $DIR/run/start_monkey_normal_local.sh &
@@ -8,7 +10,7 @@ if [ -z ${IS_PRODUCTION+x} ];
     sh $DIR/run/start_monkey_client_viewer_local.sh &
 else
     echo "running on prod";
-    FOREVER_PATH=/home/ec2-user/.nvm/versions/node/v9.4.0/bin/forever
+
     $FOREVER_PATH stopall
     sh $DIR/run/start_server_production.sh &
     sh $DIR/run/start_monkey_normal_production.sh &
